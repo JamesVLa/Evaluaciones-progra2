d168c55548261121f4dd516ecd847af96101595e
JamesVLa <91818928+JamesVLa@users.noreply.github.com>
2024-09-29 04:58:01 -0500

    Add files via upload

---
+++ Examen Parcial/1INF25 Ex1 2024-1 [Material]/MuestraVoid.h
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   MuestraVoid.h
+ * Author: cueva
+ *
+ * Created on 13 de mayo de 2024, 09:04 AM
+ */
+
+#ifndef MUESTRAVOID_H
+#define MUESTRAVOID_H
+void muestraventas(void *ventas);
+#endif /* MUESTRAVOID_H */
---
+++ Examen Parcial/1INF25 Ex1 2024-1 [Material]/PunterosMultiples1Examen01Pregunta01.h
@@ -0,0 +1,20 @@
+/* 
+ * Proyecto: Pregunta01_Examen01_Turno1_2024-1
+ * Archivo:  PunterosMultiples1Examen01Pregunta01.h
+ * Autor:    J. Miguel Guanira E.
+ *
+ * Created on 12 de mayo de 2024, 02:25 PM
+ */
+
+#ifndef PUNTEROSMULTIPLES1EXAMEN01PREGUNTA01_H
+#define PUNTEROSMULTIPLES1EXAMEN01PREGUNTA01_H
+#include <fstream>
+using namespace std;
+
+void cargarVentas (char**&, int**&,const char *);
+void pruebaDeCargaDeVentas (char **, int **, const char *);
+void cargaLibros (char ***&,const char *);
+void pruebaDeCargaDeLibros (char ***,const char *);
+
+#endif /* PUNTEROSMULTIPLES1EXAMEN01PREGUNTA01_H */
+
